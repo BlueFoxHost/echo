d309e3dd69ce1b1182c6efcd11b7e08a032ecc79
Tweak4141 <tweak@talosbot.xyz>
2023-08-30 23:49:39 -0600

    Remove NGINX, migrate HTTPS to NodeJS's HTTPS module.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-const http = require('http');
-const fs = require('fs');
+const https = require('node:https');
+const http = require('node:http');
+const fs = require('node:fs');
 const path = require('path');
+const { handleNotFound, handleRedirect, handleMethodNotAllowed, getRouteHandlerName, sanitizeToken } = require('./utils');
+
 const HOST = process.env.HOST|| '0.0.0.0';
-const PORT = process.env.PORT || 8080;
-const { handleNotFound, handleRedirect, handleMethodNotAllowed, getRouteHandlerName } = require('./utils');
+const HTTP_PORT = process.env.HTTP_PORT || 80;
+const HTTPS_PORT = process.env.HTTPS_PORT || 3000;
+const CHALLENGE_DIRECTORY = process.env.CHALLENGE_DIRECTORY || '/var/www/html/.well-known/acme-challenge';
+const DOMAIN = process.env.DOMAIN;
+if (!DOMAIN) throw new Error('CRITICAL ERROR: NO DOMAIN VARIABLE SPECIFIED IN ENV.')
+
+const options = {
+  cert: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/fullchain.pem`),
+  key: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/privkey.pem`),
+};
 
 function handleRequest(req, res, routeHandlers) {
   
@@ -22,7 +33,29 @@ function handleRequest(req, res, routeHandlers) {
   if (method !== "GET") return handleMethodNotAllowed(res); // GET only from this point on.
 
   if (url === '/') return handleRedirect(req, res, 'https://bluefoxhost.com');
-
+  if (req.url.startsWith('/.well-known/acme-challenge/')) {
+    const challengeToken = req.url.split('/').pop(); // Extract the challenge token from the URL
+    const sanitizedToken = sanitizeToken(challengeToken);
+    if (!sanitizedToken) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.end('Bad Request');
+      return;
+    }
+    
+    const filePath = path.join(CHALLENGE_DIRECTORY, sanitizedToken);
+    
+    // Read the challenge response from the file
+    fs.readFile(filePath, 'utf8', (err, challengeResponse) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(challengeResponse);
+      }
+    });
+  }
+  
   const routeHandlerName = getRouteHandlerName(url);
   const routeHandler = routeHandlers[routeHandlerName];
   
@@ -33,7 +66,7 @@ function handleRequest(req, res, routeHandlers) {
   }
 }
 
-const server = http.createServer((req, res) => {
+const httpServer = http.createServer((req, res) => {
 
   const routeHandlers = loadRouteHandlers();
   handleRequest(req, res, routeHandlers);
@@ -41,9 +74,21 @@ const server = http.createServer((req, res) => {
 });
 
 
-server.listen(PORT, HOST, () => {
+httpServer.listen(PORT, HOST, () => {
   console.log(`Server is listening on ${PORT}.\nServer is available at http://${HOST}:${PORT}`);
 });
+
+const httpsServer = http.createServer(options, (req, res) => {
+
+  const routeHandlers = loadRouteHandlers();
+  handleRequest(req, res, routeHandlers);
+
+});
+
+httpsServer.listen(PORT, HOST, () => {
+  console.log(`HTTPS Server is listening on ${PORT}.\nHTTPS Server is available at https://${DOMAIN}:${PORT}`);
+});
+
 
 function loadRouteHandlers() {
   const routeHandlers = {};
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const CHALLENGE_DIRECTORY = process.env.CHALLENGE_DIRECTORY || '/var/www/html/.well-known/acme-challenge';
+
 function handleRedirect(req, res, location) {
     res.writeHead(301, { 'Location': location });
     res.end();
@@ -18,9 +21,33 @@ function getRouteHandlerName(url) {
     return sanitizedUrl.substr(1);
 };
   
+function sanitizeToken(token) {
+    // Prevent poison null bytes
+    if (token.indexOf('\0') !== -1) {
+      return null;
+    }
+  
+    // Whitelist characters
+    if (!/^[a-zA-Z0-9]+$/.test(token)) {
+      return null;
+    }
+  
+    // Prevent directory traversal
+    const rootDirectory = path.resolve('/var/www/html'); // Get absolute path
+    const safePath = path.normalize(token).replace(/^(\.\.(\/|\\|$))+/, '');
+    const filename = path.join(CHALLENGE_DIRECTORY, safePath);
+
+    if (filename.indexOf(rootDirectory) !== 0) {
+      return null;
+    }
+  
+    return safePath;
+}
+
 module.exports = { 
     handleNotFound, 
     handleRedirect,
     handleMethodNotAllowed,
-    getRouteHandlerName
+    getRouteHandlerName,
+    sanitizeToken
 };
